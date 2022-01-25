eb06341c7b3bd4366a07668e29239ee15824b31b
Olivier Provost <olivier.provost@omnirobotic.com>
2022-01-25 15:42:38 -0500

    added main setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+import subprocess
+
+class InstallLocalPackage(install):
+    def run(self):
+        install.run(self)
+        subprocess.call(
+            "python models/ops/setup.py install", shell=True
+        )
+
+
+setup(
+    name='deformable-detr',
+    version='0.0.0',
+    author="fundamentalvision",
+    packages=find_packages(),
+    cmdclass={ 'install': InstallLocalPackage },
+)
