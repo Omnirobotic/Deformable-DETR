224233a864f0af88e66f63f1f95dd12cbecb4bdc
Olivier Provost <olivier.provost@omnirobotic.com>
2022-01-26 08:05:02 -0500

    removed old packages

--- deformable/models/ops/compile/setup.py
+++ deformable/models/ops/compile/setup.py
@@ -65,7 +65,6 @@ setup(
     author="Weijie Su",
     url="https://github.com/fundamentalvision/Deformable-DETR",
     description="PyTorch Wrapper for CUDA Functions of Multi-Scale Deformable Attention",
-    packages=find_packages(exclude=("configs", "tests", "compile")),
     ext_modules=get_extensions(),
     cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
 )
